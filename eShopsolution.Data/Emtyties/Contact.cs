7740030da01266a918558fdc37ba317d6ad89438
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-03-14 23:34:34 +0700

    #7-addclass

---
+++ eShopsolution.Data/Emtyties/Contact.cs
@@ -0,0 +1,21 @@
+﻿using eShopsolution.Data.Enums;
+
+using eShopSolution.Data.Enums;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Contact
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Message { set; get; }
+        public Status Status { set; get; }
+
+    }
+}
---
+++ eShopsolution.Data/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopsolution.Data.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
