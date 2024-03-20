d548bb868510ef85b780c6d38aea168bb09b0c44
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-03-20 21:02:49 +0700

    category_cart-SQL

---
+++ eShopsolution.Data/SQL/CategorySQL.cs
@@ -0,0 +1,21 @@
+﻿USE[FirstWebAppCore]
+GO
+
+/****** Object:  Table [dbo].[Category]    Script Date: 3/20/2024 9:01:09 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Category](
+
+[Id][int] NULL,
+	[SortOrder] [int] NULL,
+	[IsShowOnHome] [nchar](10) NULL,
+	[ParentId] [int] NULL,
+	[Status] [nchar](10) NULL
+) ON[PRIMARY]
+GO
+
+
