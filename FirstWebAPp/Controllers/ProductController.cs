f1533e129e9726947a546711dfe57930370de3f6
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-03-26 01:02:21 +0700

    product

---
+++ FirstWebAPp/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using eShopSolution.Data.Entities;
+using eShopSolution.Application;
+using System;
+
+namespace eShopSolution.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAllProducts")]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _productService.GetAllProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("GetProductById/{id}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPut("EditProduct/{id}")]
+        public async Task<IActionResult> EditProduct(int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingProduct = await _productService.GetProductById(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            await _productService.UpdateProduct(product);
+            return NoContent();
+        }
+
+        [HttpDelete("DeleteProduct/{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var productToDelete = await _productService.GetProductById(id);
+            if (productToDelete == null)
+            {
+                return NotFound();
+            }
+
+            await _productService.DeleteProduct(id);
+            return NoContent();
+        }
+    }
+}
