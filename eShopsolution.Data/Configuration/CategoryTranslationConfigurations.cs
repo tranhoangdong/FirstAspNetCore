e53e5a71be5b2491c60c3c97c939bf0c7d75da4d
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-04-02 20:15:16 +0700

    Update..

--- eShopsolution.Data/Configuration/CategoryTranslationConfigurations.cs
+++ eShopsolution.Data/Configuration/CategoryTranslationConfigurations.cs
@@ -1,32 +1,26 @@
-﻿using eShopsolution.Data.Enums;
-
+﻿
 using eShopSolution.Data.Entities;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace eShopSolution.Data.Configurations
 {
-    public class CategoryTranslationConfigurations : IEntityTypeConfiguration<CategoryTranslation>
+    public class CategoryTranslationConfiguration : IEntityTypeConfiguration<CategoryTranslation>
     {
         public void Configure(EntityTypeBuilder<CategoryTranslation> builder)
         {
             builder.ToTable("CategoryTranslation");
-
             builder.HasKey(x => x.Id);
-
             builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.CategoryId).IsRequired();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.SeoDescription).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.SeoTitle).IsRequired().HasMaxLength(255);
+            builder.Property(x => x.LanguageId).IsRequired().HasMaxLength(10);
 
-         
-            builder.HasOne(x => x.Category)
-                   .WithMany(x => x.Translations)
-                   .HasForeignKey(x => x.CategoryId);
+            builder.Property(x => x.SeoAlias).IsRequired().HasMaxLength(255); 
 
-            builder.Property(x => x.Status).HasDefaultValue(Status.Active);
         }
     }
 }
--- eShopsolution.Data/Emtyties/CategoryTranslation.cs
+++ eShopsolution.Data/Emtyties/CategoryTranslation.cs
@@ -1,33 +1,13 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace eShopSolution.Data.Configurations
+﻿namespace eShopSolution.Data.Entities
 {
-    public class CategoryTranslationConfigurations : IEntityTypeConfiguration<CategoryTranslation>
-    {
-        public void Configure(EntityTypeBuilder<CategoryTranslation> builder)
-        {
-            builder.ToTable("CategoryTranslation"); 
-
-            builder.HasKey(x => x.Id); 
-
-            builder.Property(x => x.Id).UseIdentityColumn();
-
-          
-            builder.Property(x => x.CategoryId).IsRequired();
-            builder.Property(x => x.Name).IsRequired().HasMaxLength(255);
-            builder.Property(x => x.SeoDescription).IsRequired().HasMaxLength(500);
-            builder.Property(x => x.SeoTitle).IsRequired().HasMaxLength(255);
-            builder.Property(x => x.LanguageId).IsRequired().HasMaxLength(10);
-            builder.Property(x => x.SeoAlias).IsRequired().HasMaxLength(255);
-
-          
-            builder.HasOne(x => x.Category)
-                   .WithMany(x => x.Translations)
-                   .HasForeignKey(x => x.CategoryId);
-
-         
-        }
+    public class CategoryTranslation
+    {   
+        public int Id { set; get; }
+        public int CategoryId { set; get; }
+        public string Name { set; get; }
+        public string SeoDescription { set; get; }
+        public string SeoTitle { set; get; }
+        public string LanguageId { set; get; }
+        public string SeoAlias { set; get; }
     }
 }
