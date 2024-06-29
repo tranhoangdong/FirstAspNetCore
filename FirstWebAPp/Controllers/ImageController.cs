45b0ae5070556ac9b58a0e314fc7ccaf6af8f977
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-06-29 15:24:09 +0700

    #16

---
+++ FirstWebAPp/Controllers/ImageController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using eShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using eShopSolution.Application.IService;
+using eShopsolution.Data.EF;
+using Microsoft.AspNetCore.Http;
+
+namespace eShopSolution.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+        [HttpPost("UploadImage")]
+        public async Task<IActionResult> UploadImage(IFormFile file, [FromForm] string name, [FromForm] int productId)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            var image = await _imageService.SaveImageAsync(file, name, productId);
+            return Ok(new { image.ID, image.Name });
+        }
+       
+
+        [HttpGet("GetImage/{id}")]
+        public async Task<IActionResult> GetImage(int id)
+        {
+            var image = await _imageService.GetImageByIdAsync(id);
+            if (image == null)
+                return NotFound();
+
+            //return /*File(image.Data, image.ContentType, image.Name);*/
+            return Ok(new { image.ID, image.ProductId });
+        }
+
+        [HttpGet("product/{productId}")]
+        public async Task<IActionResult> GetImagesByProductId(int productId)
+        {
+            var images = await _imageService.GetImagesByProductIdAsync(productId);
+            return Ok(images);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateImage(int id, IFormFile file, [FromForm] string name, [FromForm] int productId)
+        {
+            var image = await _imageService.UpdateImageAsync(id, file, name, productId);
+            if (image == null)
+                return NotFound();
+
+            return Ok(new { image.ID, image.Name });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var result = await _imageService.DeleteImageAsync(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+
+
+
+    }
+}
+
---
+++ eShopsolution.Application/Service/ImageService.cs
@@ -0,0 +1,89 @@
+﻿using eShopsolution.Data.EF;
+using eShopSolution.Application.IService;
+using eShopSolution.Data.Entities;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Service
+{
+    public class ImageService : IImageService
+    {
+        private readonly EShopDbContext _eShopDbContext;
+
+        public ImageService(EShopDbContext eShopDbContext)
+        {
+            _eShopDbContext = eShopDbContext;
+        }
+        public async Task<Image> SaveImageAsync(IFormFile file, string name, int productId)
+        {
+            byte[] fileData;
+            using (var stream = new System.IO.MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+                fileData = stream.ToArray();
+            }
+
+            var image = new Image
+            {
+                ProductId = productId,
+                Name = name,
+                ContentType = file.ContentType,
+                Data = fileData
+            };
+
+            _eShopDbContext.Images.Add(image);
+            await _eShopDbContext.SaveChangesAsync();
+
+            return image;
+        }
+        public async Task<Image> GetImageByIdAsync(int id)
+        {
+            return await _eShopDbContext.Images.FindAsync(id);
+        }
+        public async Task<IEnumerable<Image>> GetImagesByProductIdAsync(int productId)
+        {
+            return _eShopDbContext.Images.Where(img => img.ProductId == productId).ToList();
+        }
+        public async Task<Image> UpdateImageAsync(int id, IFormFile file, string name, int productId)
+        {
+            var existingImage = await _eShopDbContext.Images.FindAsync(id);
+            if (existingImage == null)
+                return null;
+
+            if (file != null && file.Length > 0)
+            {
+                using (var stream = new System.IO.MemoryStream())
+                {
+                    await file.CopyToAsync(stream);
+                    existingImage.Data = stream.ToArray();
+                    existingImage.ContentType = file.ContentType;
+                }
+            }
+
+            existingImage.Name = name;
+            existingImage.ProductId = productId;
+
+            await _eShopDbContext.SaveChangesAsync();
+
+            return existingImage;
+        }
+        public async Task<bool> DeleteImageAsync(int id)
+        {
+            var image = await _eShopDbContext.Images.FindAsync(id);
+            if (image == null)
+                return false;
+
+            _eShopDbContext.Images.Remove(image);
+            await _eShopDbContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+
+}
