92c6b65338c7c933bec2254a1d44e2f1f59a6b4e
tranhoangdong <88124107+tranhoangdong@users.noreply.github.com>
2024-09-14 10:52:46 +0700

    update

--- ASPProjectMVC/Startup.cs
+++ ASPProjectMVC/Startup.cs
@@ -33,9 +33,7 @@ namespace ASPProjectMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<EShopDbContext>(options =>
-                       options.UseSqlServer(Configuration.GetConnectionString("eShopSolutionDb")));
-            services.AddIdentity<User, Roles>().AddEntityFrameworkStores<EShopDbContext>().AddDefaultTokenProviders();
+         
 
             services.AddControllersWithViews();
             services.AddTransient<IProductService, ProductService>();
